3aa2502ba54b0309bac405d1af45fe2a87ac19c8
Ryan <Ryan@Ryans-Air.local>
2014-05-19 19:52:05 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loogouv3::Application.config.secret_key_base = 'd832f13704f09fa47eabecc111cabc0372aee1f87ab3f44da892c7327681f2c7db7f73d318edc17bd5e959ee45ec2c18a7fbad4e6e1fdacfe0512df45cfccc7a'
