e608e740ca74fd22100269568431dac54ed2c32d
Ryan <Ryan@Ryans-Air.local>
2014-05-20 13:33:53 +0800

    delete customiation js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,8 @@
 //= require ckeditor/init
 
 
-//= require_directory ./customization/scripts
-//= require_directory ./customization/js
+// require_directory ./customization/scripts
+// require_directory ./customization/js
 //= require bootstrap
 
 
