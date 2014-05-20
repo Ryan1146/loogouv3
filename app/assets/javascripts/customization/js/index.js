eaa70df2cea64be7d6adfadd0a72f2eebad03511
Ryan <Ryan@Ryans-Air.local>
2014-05-20 13:32:44 +0800

    modify require_directory

--- app/assets/javascripts/customization/js/index.js
+++ app/assets/javascripts/customization/js/index.js
@@ -221,13 +221,12 @@ $(document).on('ready page:load', function (){
             this.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',sizingMethod='scale',src='"+ this.src +"')";
         });
     }
-
-
-
-
-    $(function(){
-    $.Body =$('body');  
-    $.Window = $(window);
+    
+});
+	
+$(function(){
+	$.Body =$('body');	
+	$.Window = $(window);
     $.collectBtn = $.Body.find('div.collectBtn');
     $.flagItem = $.Body.find('div.flagItem');
     $.navBox = $.Body.find('div.navBox');
@@ -241,8 +240,4 @@ $(document).on('ready page:load', function (){
 
     
 });
-    
-});
-	
-
 
